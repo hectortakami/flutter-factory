d08914129535df56dfdf81a256d566130f8e31dd
Duncanista <30836115+duncanista@users.noreply.github.com>
2022-03-31 17:55:08 -0600

    update event model

--- A01745993 Jordan Gonzalez/design_proposal/lib/models/event.dart
+++ A01745993 Jordan Gonzalez/design_proposal/lib/models/event.dart
@@ -1,11 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Event {
   final String uid;
   final String name;
   final String description;
   final DateTime date;
-  Map<String, dynamic> location;
-  final String ownerId;
+  Map<String, dynamic> address;
+  final String ownerUid;
   List<Map<String, dynamic>> participants;
 
-  Event(this.uid, this.name, this.description, this.date, this.location, this.ownerId, this.participants);
+  Event(
+      {required this.uid,
+      required this.name,
+      required this.description,
+      required this.date,
+      required this.address,
+      required this.ownerUid,
+      required this.participants});
+
+  factory Event.fromMap(Map<String, dynamic> data, String uid) {
+    String name = data['name'];
+    String description = data['description'];
+    DateTime date =
+        DateTime.fromMillisecondsSinceEpoch(int.parse(data['date']));
+    String ownerUid = data['ownerUid'];
+    List<Map<String, dynamic>> participants = [];
+
+    return Event(
+        uid: uid,
+        name: name,
+        description: description,
+        date: date,
+        address: data['address'],
+        ownerUid: ownerUid,
+        participants: participants);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'description': description,
+      'address': address,
+      'date': Timestamp.fromMillisecondsSinceEpoch(date.millisecondsSinceEpoch)
+          .toString(),
+      'ownerUid': ownerUid
+    };
+  }
 }
